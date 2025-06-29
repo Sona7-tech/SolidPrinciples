16fa3857d0129483aa9ada859454228438981718
Sona Ibrahimova <73877099+Sona7-tech@users.noreply.github.com>
2025-06-30 02:20:04 +0400

    Update Main.java

--- src/solidprinciples/openclosed/Main.java
+++ src/solidprinciples/openclosed/Main.java
@@ -10,4 +10,16 @@ public class Main {
         reportService.printReport(balanceReport);
         reportService.printReport(incomeReport);
     }
+
+    public class Main {
+
+    public void calculate(Report report) {
+
+        if (report instanceof IncomeReport) {
+            IncomeReport income = (IncomeReport) report;
+        } else if (report instanceof BalanceReport) {
+            BalanceReport balance = (BalanceReport) report;
+        }
+    }
+}
 }
